d2f0c1e3c3bb21ba5e06219851da6f728ee0235c
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-07-22 17:35:01 +0530

    String Buildercode added

--- String/StringMethods.java
+++ String/StringMethods.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class StringMethods {
+
+    //interning method in strings where all the same data strings are directed to the data created in heap and the new key keyword creates the new data memory in the heap and assign it to the corresponding variable;
+
     public static String subString(String input,int s,int e) {
         String ans ="";
         for(int i=s;i<e;i++) {
@@ -25,12 +28,21 @@ public class StringMethods {
         return ans;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         String arr[] = {"mango", "banana", "apple"};
-        String input = sc.nextLine();
+        String input = "Hello World";
         System.out.println("Substring is: "+subString(input, 0, 5) );
         System.out.println("Substring is: "+input.substring(0, 5) );
         System.out.println("Biggest lexicographically string: "+compareString(arr)[0]);
         System.out.println("Smallest lexicographically string: "+compareString(arr)[1]);
+
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a';ch<'z';ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        // int a = 5;
+        Integer b = 5;
+        System.out.println(/*a.toString()+*/b.toString());
     }
 }
