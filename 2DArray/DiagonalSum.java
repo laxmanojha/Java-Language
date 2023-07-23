0c55532dce9e2df59463e0a0ea66c80c27c06122
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-07-23 20:31:57 +0530

    Optimized Diagonal sum code added

--- 2DArray/DiagonalSum.java
+++ 2DArray/DiagonalSum.java
@@ -7,7 +7,7 @@ public class DiagonalSum {
             System.out.println();
         }
     }
-    public static void diagonalSumNXN(int matrix[][]) {
+    public static void diagonalSum(int matrix[][]) {
         int rowLength = matrix.length-1;
         int colLength = matrix[0].length-1;
         int sum = 0;
@@ -16,39 +16,39 @@ public class DiagonalSum {
                 if(i==j) {
                     sum+=matrix[i][j];
                 }
-                if(i==colLength-j) {
+                else if(i==colLength-j) {
                     sum+=matrix[i][j];
                 }
             }
         }
-        System.out.println("The diagonal sum NXN is: "+sum);
+        System.out.println("The diagonal sum "+matrix.length+"x"+matrix.length+" is: "+sum);
     }
-    public static void diagonalSumNXM(int matrix[][]) {
-        int rowLength = matrix.length-1;
-        int colLength = matrix[0].length-1;
+    public static void optimizedDiagonalSum(int matrix[][]) {
         int sum = 0;
-        for(int i=0;i<=rowLength;i++) {
-            for(int j=0;j<=colLength;j++) {
-                if(i==j && i==colLength-j) {
-                    sum+=matrix[i][j];
-                    continue;
-                }
-                if(i==j) {
-                    sum+=matrix[i][j];
-                }
-                if(i==colLength-j) {
-                    sum+=matrix[i][j];
-                }
-            }
+        // int k=matrix.length-1;
+        for(int i=0;i<matrix.length;i++) {
+            sum+=matrix[i][i];
+            if(i != matrix.length-1-i)
+                sum+=matrix[i][matrix.length-1-i];
+            // if(i==k) {
+            //     sumofSD+=matrix[i][i];
+            //     k--;
+            //     continue;
+            // }
+            // sumofPD+=matrix[i][i];
+            // sumofSD+=matrix[i][k];
+            // k--;
         }
-        System.out.println("The diagonal sum of NXM is: "+sum);
+        System.out.println("The Optimized diagonal sum "+matrix.length+"x"+matrix.length+" is: "+sum);
     }
     public static void main(String[] args) {
-        int matrixNXN[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        int matrixNXM[][] = {{1,2,3,},{5,6,7,},{9,10,11,}};
-        printMatrix(matrixNXN);
-        diagonalSumNXN(matrixNXN);
-        printMatrix(matrixNXM);
-        diagonalSumNXM(matrixNXM);
+        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int matrix2[][] = {{1,2,3,},{5,6,7,},{9,10,11,}};
+        printMatrix(matrix);
+        printMatrix(matrix2);
+        diagonalSum(matrix);
+        diagonalSum(matrix2);
+        optimizedDiagonalSum(matrix);
+        optimizedDiagonalSum(matrix2);
     }
 }
