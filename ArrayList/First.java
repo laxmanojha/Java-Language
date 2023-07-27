61295911714703cf65d1468da67e831987f35ac9
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-07-27 22:19:32 +0530

    Add, Get, Set, Remove, Contains Method of ArrayList added

--- ArrayList/First.java
+++ ArrayList/First.java
@@ -3,7 +3,31 @@ import java.util.ArrayList;
 public class First {
     public static void main(String[] args) {
         ArrayList<Integer>list = new ArrayList<>();
-        ArrayList<String>list2 = new ArrayList<>();
-        ArrayList<Boolean>list3 = new ArrayList<>();
+        // ArrayList<String>list2 = new ArrayList<>();
+        // ArrayList<Boolean>list3 = new ArrayList<>();
+
+        //Add method O(1)
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(1,10); //O(n)
+        System.out.println("List: "+list);
+
+        //Get method O(1) 
+        System.out.println("Get element: "+list.get(4));
+
+        //Set method O(n)
+        list.set(1, 5);
+        System.out.println("Set element: "+list);
+
+        //Remove method O(n)
+        // int a = list.remove(1);
+        System.out.println("removed element: "+list.remove(1));
+        System.out.println("List: "+list);
+
+        //Contains element O(n)
+        System.out.println("Contains: "+list.contains(10));
+        System.out.println("Contains: "+list.contains(2));
     }
 }
