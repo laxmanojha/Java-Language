c0f5fd2a861bee2491bd82e38e5a36db0fc1b9a5
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-07-28 09:23:35 +0530

    brute force and two pointer approach added

--- ArrayList/pairSum.java
+++ ArrayList/pairSum.java
@@ -2,12 +2,39 @@ import java.util.*;
 
 public class pairSum {
 
+    public static boolean bruteForce(ArrayList<Integer>list,int target) {
+        for(int i=0;i<list.size();i++) {
+            for(int j=i+1;j<list.size();j++) {
+                if((list.get(i)+list.get(j))==target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean optimizedPairSum(ArrayList<Integer>list, int target) {
+        int lp = 0;
+        int rp = list.size()-1;
+        while(lp<rp) {
+            if((list.get(lp)+list.get(rp)) == target) {
+                return true;
+            }
+            else if((list.get(lp)+list.get(rp)) < target) {
+                lp++;
+            }
+            else {
+                rp--;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         ArrayList<Integer>list = new ArrayList<>(
             List.of(1,2,4,5,6)
         );
         list.add(2,3);
-
+        System.out.println("Pair sum exists: "+bruteForce(list, 12));
+        System.out.println("Pair sum exists: "+optimizedPairSum(list, 12));
 
 
 
