e71dc009fa94851f675f7eb52f6e7129beb79fac
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-07-28 09:14:26 +0530

    Methods of initializing an ArrayList added

---
+++ ArrayList/pairSum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class pairSum {
+
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>(
+            List.of(1,2,4,5,6)
+        );
+        list.add(2,3);
+
+
+
+
+        // ArrayList<Integer>list2 = new ArrayList<>(
+        //     Arrays.asList(1,2,3,4,5,6)
+        // );
+        // ArrayList<Integer>list2 = new ArrayList<>(){
+        //     {
+        //         add(1);
+        //         add(2);
+        //         add(3);
+        //     }
+        // };
+        // System.out.println(list+"\n"+list2);
+    }
+}
