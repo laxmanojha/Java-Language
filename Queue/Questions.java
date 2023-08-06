f9b447fd89075ed2ad5d3061b59fcd1c2fb33c46
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-08-06 19:03:25 +0530

    First Non repeating occurenece letter in stream of string solved using stack

---
+++ Queue/Questions.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Questions {
+    public static String firstNonRepeatingOccurence(String input) {
+        int[] freq = new int[26]; // a a b c c x b
+        StringBuilder ans = new StringBuilder("");
+        Queue<Character> queue = new LinkedList<>();
+        for(int i=0;i<input.length();i++) {
+            boolean flag = true;
+            freq[input.charAt(i)-97]++;
+            queue.add(input.charAt(i));
+            while(!queue.isEmpty()) {
+                if(freq[queue.peek()-97] == 1) {
+                    ans.append(queue.peek()); // a -1 b b b b x
+                    flag = false;
+                    break;
+                }
+                else {
+                    queue.remove();
+                }
+            }
+            if(flag) {
+                ans.append(-1);
+            }
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String input = "aabccxb";
+        System.out.println(firstNonRepeatingOccurence(input));
+    }
+}
