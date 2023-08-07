95d606462b760250b7da356b68c872c22e90a8fa
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-08-07 12:58:16 +0530

    Queue implementation using Deque

--- Queue/DequeIntro.java
+++ Queue/DequeIntro.java
@@ -22,10 +22,45 @@ public class DequeIntro {
                 return -1;
         }
     }
+    static class QueueByDeque {
+        Deque<Integer>deque = new LinkedList<>();
+        public boolean isEmpty() {
+            return deque.isEmpty();
+        }
+        public void add(int data) {
+            deque.addLast(data);
+        }
+        public int remove() {
+            if(!deque.isEmpty())
+                return deque.removeFirst();
+            else 
+                return -1;
+        }
+        public int peek() {
+            if(!deque.isEmpty())
+                return deque.getFirst();
+            else 
+                return -1;
+        }
+        public int size() {
+            return deque.size();
+        }
+    }
     public static void main(String[] args) {
 
+        // Queue implemendted using Deque
+        QueueByDeque queue = new QueueByDeque();
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.peek());
+        queue.add(10);
+        queue.add(20);
+        System.out.println(queue.peek());
+        System.out.println(queue.remove());
+        System.out.println(queue.peek());
+        System.out.println(queue.isEmpty());
+
         // Stack implemendted using Deque
-        StackByDeque stack = new StackByDeque();
+        /*StackByDeque stack = new StackByDeque();
         System.out.println(stack.isEmpty());
         System.out.println(stack.peek());
         stack.push(10);
@@ -33,7 +68,7 @@ public class DequeIntro {
         System.out.println(stack.peek());
         System.out.println(stack.pop());
         System.out.println(stack.peek());
-        System.out.println(stack.isEmpty());
+        System.out.println(stack.isEmpty());*/
 
 
         /*Deque<Integer>deque = new LinkedList<>();
