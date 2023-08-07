dec3873f30657a9c7344865dfb1760bb24bb1027
Laxman Prasad Ojha <lpojha2001@gmail.com>
2023-08-07 12:16:50 +0530

    InterLeave Halves solved using three Queues

--- Queue/Questions.java
+++ Queue/Questions.java
@@ -29,8 +29,38 @@ public class Questions {
         }
         return ans.toString();
     }
+    public static void InterLeaveHalves(Queue<Integer>firstHalf, Queue<Integer>secondHalf,Queue<Integer>ans) {
+        if(firstHalf.isEmpty() && secondHalf.isEmpty()) {
+            return;
+        }
+
+        ans.add(firstHalf.remove());
+        ans.add(secondHalf.remove());
+        InterLeaveHalves(firstHalf, secondHalf, ans);
+    }
     public static void main(String[] args) {
+
+        Queue<Integer> ans = new LinkedList<>();
+        Queue<Integer> firstHalf = new LinkedList<>();
+        Queue<Integer> secondHalf = new LinkedList<>();
+        for(int i=1;i<=10;i++) {
+            if(i<=5) {
+                firstHalf.add(i);
+            }
+            else {
+                secondHalf.add(i);
+            }
+        }
+        InterLeaveHalves(firstHalf, secondHalf, ans);
+        for(int i=0;i<ans.size();i++) {
+            int temp = ans.remove();
+            ans.add(temp);
+            System.out.print(temp+" ");
+
+        }
+
+        /*Question 
         String input = "aabccxb";
-        System.out.println(firstNonRepeatingOccurence(input));
+        System.out.println(firstNonRepeatingOccurence(input));*/
     }
 }
